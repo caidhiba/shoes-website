572073ba2343d5a92762bd6904c99fa02f6db12d
hiba <caidhiba05@gmail.com>
2024-06-19 15:00:07 +0100

    adding features

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const wrapper = document.querySelector(".sliderWrapper");
-const menuItems = document.querySelectorAll("menuItem");
-// wrapper.style.transform = "translateX()"
+const menuItems = document.querySelectorAll(".menuItem");
+ wrapper.style.transform = "translateX(-400vw)";
 
 menuItems.forEach((item, index)=>{
     item.addEventListener("click",()=>{
-         wrapper.style.transform = `translateX(%{-100 * index}vw)`;
+         wrapper.style.transform = `translateX(${-100 * index}vw)`;
+        // console.log(index);
     });
 });
